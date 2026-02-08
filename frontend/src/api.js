6bda7fd1934bbf5ee7348cea8a7d670a07c2a2fb
PrajaktaSarkhel <prajaktasarkhel@gmail.com>
2026-02-09 00:18:08 +0530

    Added backend URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,9 @@
-import axios from 'axios';
+import axios from "axios";
 
 const API = axios.create({
-    // Use the Render URL if it exists, otherwise use localhost
-    baseURL: process.env.REACT_APP_BACKEND_URL || "http://localhost:5001/api"
+  baseURL:
+    process.env.REACT_APP_API_BASE_URL ||
+    "http://localhost:5001/api",
 });
 
 export default API;
